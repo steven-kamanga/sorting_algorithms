92e07566f5ccf5cad2cba1187b9cf1a963a8fadb
drplxrd <kamangasteven00@gmail.com>
2023-04-29 14:25:50 +0200

    styled

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,32 +10,32 @@
 
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, inner, outer;
-    int temp;
+	size_t gap = 1, inner, outer;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+	while (gap < size / 3)
+		gap = gap *3 + 1;
 
-    while (gap > 0)
-    {
-        for (outer = gap; outer < size; outer++)
-        {
-            temp = array[outer];
-            inner = outer;
+	while (gap > 0)
+	{
+		for (outer = gap; outer < size; outer++)
+		{
+			temp = array[outer];
+			inner = outer;
 
-            while (inner > gap - 1 && array[inner - gap] >= temp)
-            {
-                array[inner] = array[inner - gap];
-                inner -= gap;
-            }
+			while (inner > gap - 1 && array[inner - gap] >= temp)
+			{
+				array[inner] = array[inner - gap];
+				inner -= gap;
+			}
 
-            array[inner] = temp;
-        }
+			array[inner] = temp;
+		}
 
-        gap = (gap - 1) / 3;
-        print_array(array, size);
-    }
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
 }
