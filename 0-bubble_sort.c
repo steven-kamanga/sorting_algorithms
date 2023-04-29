ac63fe76f7b4c4bc8bd738a90684f6deb6e078af
drplxrd <kamangasteven00@gmail.com>
2023-04-29 13:41:05 +0200

    Styled

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,33 +1,31 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sort array of integers in ascending order using basic for loops
- * @array: the array
- * @size: The size of the array
- *
- * Return: void
+ *bubble_sort - Sorts an array of integers in ascending order
+ *@array: The array to be sorted
+ *@size: The size of the array
  */
-
 void bubble_sort(int *array, size_t size)
 {
-    size_t inner_loop, outer_loop;
-    int temp;
+	size_t i, j;
+	int tmp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (inner_loop = 0; inner_loop < size - 1; inner_loop++)
-    {
-        for (outer_loop = 1; outer_loop < size - inner_loop; outer_loop++)
-        {
-            if (array[outer_loop - 1] > array[outer_loop])
-            {
-                temp = array[outer_loop - 1];
-                array[outer_loop - 1] = array[outer_loop];
-                array[outer_loop] = temp;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{ 	/*Swap the two elements */
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 
-                print_array(array, size);
-            }
-        }
-    }
+				/*Print the array after each swap */
+				print_array(array, size);
+			}
+		}
+	}
 }
